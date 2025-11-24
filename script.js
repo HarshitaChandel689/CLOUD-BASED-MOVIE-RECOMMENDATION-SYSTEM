8c10eb3303ae06aba25388823a6b2b22f82dac76
HarshitaChandel689 <231031025@juitsolan.in>
2025-11-24 14:24:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,284 @@
+const movieData = {
+
+    HORROR: {
+        ENGLISH: [
+            {
+                title: "EXORCIST",
+                poster: "https://upload.wikimedia.org/wikipedia/en/7/7b/Exorcist_ver2.jpg",
+                trailer: "https://www.youtube.com/watch?v=YDGw1MTEe9k"
+            },
+            {
+                title: "IT",
+                poster: "https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=FnCdOQsX5kc"
+            },
+            {
+                title: "VERONICA",
+                poster: "https://upload.wikimedia.org/wikipedia/en/0/00/Veronica_%282017_Spanish_film%29.jpg",
+                trailer: "https://www.youtube.com/watch?v=wNlMIvKnjOQ"
+            },
+            {
+                title: "THE NUN",
+                poster: "https://upload.wikimedia.org/wikipedia/en/3/34/TheNunPoster.jpg",
+                trailer: "https://www.youtube.com/watch?v=pzD9zGcUNrw"
+            }
+        ],
+        HINDI: [
+            {
+                title: "TUMBBAD",
+                poster: "https://upload.wikimedia.org/wikipedia/en/4/41/Tumbbad_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=YGIcZrUBY0k"
+            },
+            {
+                title: "SHAITAAN",
+                poster: "https://upload.wikimedia.org/wikipedia/en/f/f0/Shaitaan_2024_film_theatrical_poster.jpeg",
+                trailer: "https://www.youtube.com/watch?v=Yxe-mIVIwM4"
+            },
+            {
+                title: "KANTARA",
+                poster: "https://upload.wikimedia.org/wikipedia/en/8/84/Kantara_poster.jpeg",
+                trailer: "https://www.youtube.com/watch?v=6oKFao0aISA"
+            },
+            {
+                title: "1920",
+                poster: "https://upload.wikimedia.org/wikipedia/en/a/a9/1920_Poster.JPG",
+                trailer: "https://www.youtube.com/watch?v=yy1Fdp_h4mM"
+            }
+        ]
+    },
+
+    FICTION: {
+        ENGLISH: [
+            {
+                title: "AVENGERS",
+                poster: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=eOrNdBpGMv8"
+            },
+            {
+                title: "INTERSTELLAR",
+                poster: "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+            },
+            {
+                title: "DUNE",
+                poster: "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
+                trailer: "https://www.youtube.com/watch?v=n9xhJrPXop4"
+            },
+            {
+                title: "MAN OF STEEL",
+                poster: "https://upload.wikimedia.org/wikipedia/en/5/50/Man_of_Steel_%28film%29_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=T6DJcgm3wNY"
+            }
+        ],
+        HINDI: [
+            {
+                title: "BRAHMASTRA",
+                poster: "https://upload.wikimedia.org/wikipedia/en/e/ea/Brahmastra_Part_One_Shiva.jpg",
+                trailer: "https://www.youtube.com/watch?v=BUjXzrgntcY"
+            },
+            {
+                title: "RA.ONE",
+                poster: "https://upload.wikimedia.org/wikipedia/en/5/58/Ra.Oneposter.jpg",
+                trailer: "https://www.youtube.com/watch?v=pBud7byobX0"
+            },
+            {
+                title: "A FLYING JATT",
+                poster: "https://upload.wikimedia.org/wikipedia/en/5/57/A_Flying_Jatt_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=1J9Z0qhKZqo"
+            },
+            {
+                title: "KRRISH",
+                poster: "https://upload.wikimedia.org/wikipedia/en/9/95/Krrish_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=3qa3L9rTEG0"
+            }
+        ]
+    },
+
+    COMEDY: {
+        ENGLISH: [
+            {
+                title: "THE DICTATOR",
+                poster: "https://upload.wikimedia.org/wikipedia/en/4/4b/The_Dictator_Poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=cYplvwBvGA4"
+            },
+            {
+                title: "FREE GUY",
+                poster: "https://upload.wikimedia.org/wikipedia/en/1/1c/Free_Guy_2021_Poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=X2m-08cOAbc"
+            },
+            {
+                title: "CENTRAL INTELLIGENCE",
+                poster: "https://upload.wikimedia.org/wikipedia/en/8/8a/CentralIntelligencePoster.jpg",
+                trailer: "https://www.youtube.com/watch?v=MxEw3elSJ8M"
+            },
+            {
+                title: "BORAT",
+                poster: "https://upload.wikimedia.org/wikipedia/en/3/39/Borat_ver2.jpg",
+                trailer: "https://www.youtube.com/watch?v=dL6_G1z6ymw"
+            }
+        ],
+        HINDI: [
+            {
+                title: "HOUSEFULL",
+                poster: "https://upload.wikimedia.org/wikipedia/en/3/32/Housefull_Poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=oqPZHVekBZ0"
+            },
+            {
+                title: "DE DANA DAN",
+                poster: "https://upload.wikimedia.org/wikipedia/en/2/2b/Dedanadan.jpg",
+                trailer: "https://www.youtube.com/watch?v=VMOlmpDS5O4"
+            },
+            {
+                title: "GOLMAAL RETURNS",
+                poster: "https://upload.wikimedia.org/wikipedia/en/d/dc/Golmaalreturns1.jpg",
+                trailer: "https://www.youtube.com/watch?v=jAw46WCmTN4"
+            },
+            {
+                title: "DHOL",
+                poster: "https://upload.wikimedia.org/wikipedia/en/2/20/Dhol1.jpg",
+                trailer: "https://www.youtube.com/watch?v=vqH2cV1T9WE"
+            }
+        ]
+    },
+
+    FAMILY: {
+        ENGLISH: [
+            {
+                title: "THE KARATE KID",
+                poster: "https://upload.wikimedia.org/wikipedia/en/d/d5/Karate_kid_ver2.jpg",
+                trailer: "https://www.youtube.com/watch?v=XY8amUImEu0"
+            },
+            {
+                title: "DUMBO",
+                poster: "https://upload.wikimedia.org/wikipedia/en/9/91/Dumbo_%282019_film%29.png",
+                trailer: "https://www.youtube.com/watch?v=7NiYVoqBt-8"
+            },
+            {
+                title: "DOLITTLE",
+                poster: "https://upload.wikimedia.org/wikipedia/en/1/1f/Dolittle_%282020_film_poster%29.png",
+                trailer: "https://www.youtube.com/watch?v=FEf412bSPLs"
+            },
+            {
+                title: "SHREK",
+                poster: "https://upload.wikimedia.org/wikipedia/en/7/7b/Shrek_%282001_animated_feature_film%29.jpg",
+                trailer: "https://www.youtube.com/watch?v=CwXOrWvPBPk"
+            }
+        ],
+        HINDI: [
+            {
+                title: "BAGHBAN",
+                poster: "https://upload.wikimedia.org/wikipedia/en/7/73/Baghban_2003_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=TNfAn6hDGFw"
+            },
+            {
+                title: "SWADES",
+                poster: "https://upload.wikimedia.org/wikipedia/en/8/85/Swades_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=vc7AZNWvs0M"
+            },
+            {
+                title: "TAARE ZAMEEN PAR",
+                poster: "https://upload.wikimedia.org/wikipedia/en/b/b4/Taare_Zameen_Par_Like_Stars_on_Earth_poster.png",
+                trailer: "https://www.youtube.com/watch?v=tn_2Ie_jtX8"
+            },
+            {
+                title: "WELCOME",
+                poster: "https://upload.wikimedia.org/wikipedia/en/f/f4/Welcome_poster_2007.jpg",
+                trailer: "https://www.youtube.com/watch?v=s044Cs0gTJQ"
+            }
+        ]
+    },
+
+    ROMANCE: {
+        ENGLISH: [
+            {
+                title: "TITANIC",
+                poster: "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png",
+                trailer: "https://www.youtube.com/watch?v=2e-eXJ6HgkQ"
+            },
+            {
+                title: "THE FAULT IN OUR STARS",
+                poster: "https://upload.wikimedia.org/wikipedia/en/4/41/The_Fault_in_Our_Stars_%28Official_Film_Poster%29.png",
+                trailer: "https://www.youtube.com/watch?v=9ItBvH5J6ss"
+            },
+            {
+                title: "REMEMBER ME",
+                poster: "https://upload.wikimedia.org/wikipedia/en/b/b8/Remember_me_film_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=oRUo4cWejYM"
+            },
+            {
+                title: "LA LA LAND",
+                poster: "https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png",
+                trailer: "https://www.youtube.com/watch?v=0pdqf4P9MB8"
+            }
+        ],
+        HINDI: [
+            {
+                title: "AASHIQUI 2",
+                poster: "https://upload.wikimedia.org/wikipedia/en/f/f3/Aashiqui_2_%28Poster%29.jpg",
+                trailer: "https://www.youtube.com/watch?v=FyXXgpPqe6w"
+            },
+            {
+                title: "HUM TUM",
+                poster: "https://upload.wikimedia.org/wikipedia/en/9/92/Hum_Tum_%28film%29_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=eNOmLIxv7U4"
+            },
+            {
+                title: "JAB TAK HAI JAAN",
+                poster: "https://upload.wikimedia.org/wikipedia/en/f/f9/Jab_Tak_Hai_Jaan_Poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=v0UXgoJ9Shg"
+            },
+            {
+                title: "FANNA",
+                poster: "https://upload.wikimedia.org/wikipedia/en/4/49/Fanaa_2006_poster.jpg",
+                trailer: "https://www.youtube.com/watch?v=kofrlCHyiaU"
+            }
+        ]
+    }
+};
+
+
+function recommendMovies() {
+    const genre = document.getElementById("genre").value;
+    const language = document.getElementById("language").value;
+    const movieList = document.getElementById("movies");
+    const modal = document.getElementById("popupModal");
+
+    if (genre && language) {
+        const movies = movieData[genre][language];
+
+        movieList.innerHTML =
+            `<p><strong>Recommended ${language} ${genre.toLowerCase()} movies:</strong></p>`;
+
+        movieList.innerHTML +=
+            `<div class="movie-grid">${movies.map(movie =>
+                `<div class="movie-card">
+                    <a href="${movie.trailer}" target="_blank">
+                        <img src="${movie.poster}" alt="${movie.title} Poster">
+                    </a>
+                    <div class="movie-title">${movie.title}</div>
+                </div>`
+            ).join('')}</div>`;
+    } else {
+        movieList.innerHTML = `<p>Please select both genre and language.</p>`;
+    }
+
+    modal.classList.add("show");
+}
+
+function refreshPage() {
+    document.getElementById("genre").value = "";
+    document.getElementById("language").value = "";
+    document.getElementById("movies").innerHTML = "";
+    closeModal();
+}
+
+function closeModal() {
+    document.getElementById("popupModal").classList.remove("show");
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("popupModal");
+    if (event.target === modal) {
+        closeModal();
+    }
+};
